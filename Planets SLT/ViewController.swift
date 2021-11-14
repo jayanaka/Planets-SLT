8bb2d89f9811c6be5a6f3eeecdcc7b42a94b3624
Chathura Jayanaka <chathura.gamage@layoutindex.lk>
2021-11-14 08:05:59 +0530

    Initial Commit

---
+++ Planets SLT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planets SLT
+//
+//  Created by Chathura Jayanaka on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
